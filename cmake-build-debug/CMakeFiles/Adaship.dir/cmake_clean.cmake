c9f3e74c32d3faee7a413ee100fb99423f582aa5
Mariana Stoiovici <mstoiovici@google.com>
2021-03-25 11:25:27 +0000

    first commit

---
+++ cmake-build-debug/CMakeFiles/Adaship.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Adaship"
+  "Adaship.pdb"
+  "CMakeFiles/Adaship.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Adaship.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
